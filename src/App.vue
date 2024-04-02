48b4f6959acf2b277be35afdd02e22ed3d35a589
gjy1025 <2787912196@qq.com>
2024-04-02 14:26:52 +0800

    4.2

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+ 
+  <Test></Test>
+</template>
+
+<style scoped>
+
+</style>
