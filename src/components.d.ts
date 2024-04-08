383b6bb0bf84a0f35ff00b9180a930415ec5b0a8
gjy1025 <2787912196@qq.com>
2024-04-08 19:10:37 +0800

    4.8

--- src/components.d.ts
+++ src/components.d.ts
@@ -28,5 +28,6 @@ declare module 'vue' {
     VanForm: typeof import('vant/es')['Form']
     VanIcon: typeof import('vant/es')['Icon']
     VanNavBar: typeof import('vant/es')['NavBar']
+    VanUploader: typeof import('vant/es')['Uploader']
   }
 }
