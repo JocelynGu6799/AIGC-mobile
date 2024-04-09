71e578a95ba54f884f5000f2d645d633404d3695
KBASE33 <1259291621@qq.com>
2024-04-09 10:20:43 +0800

    jbx

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,12 @@ import Login from '@/views/Login.vue'
 import Select from '@/views/Select.vue'
 import NotFound from '@/views/NotFound.vue'
 import Logo from '@/views/Logo/index.vue'
-import LogoView from '@/views/Logo/imgView.vue'
 import Pack from '@/views/Pack/index.vue'
 
 import PackView from '@/views/Pack/imgView.vue'
 
 import Poster from '@/views/Poster/index.vue'
+import PosterView from '@/views/Poster/imgView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 
@@ -36,11 +36,11 @@ const router = createRouter({
       component: Logo
     },
 
-    {
-      path: '/logo/view',
-      name: 'logoview',
-      component: LogoView
-    },
+    // {
+    //   path: '/logo/view',
+    //   name: 'logoview',
+    //   component: Pack
+    // },
 
     {
       path: '/pack',
@@ -58,11 +58,11 @@ const router = createRouter({
       name: 'poster',
       component: Poster
     },
-    // {
-    //   path: '/poster/view',
-    //   name: 'posterview',
-    //   component: Pack
-    // },
+    {
+      path: '/poster/view',
+      name: 'posterview',
+      component: PosterView
+    },
 
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
     // {
